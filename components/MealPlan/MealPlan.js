f138916891119c6e55d262a029ca1c3a00b42add
lennartsargezian <Lennart_1997@hotmail.com>
2023-10-25 16:26:09 -0700

    UI improvment for Mealplan screen

--- components/MealPlan/MealPlan.js
+++ components/MealPlan/MealPlan.js
@@ -1,7 +1,16 @@
-import {View, StyleSheet, Text, Button} from 'react-native';
+import {View, StyleSheet, Text, ScrollView, Image, Platform, Dimensions, FlatList} from 'react-native';
 import COLORS from "../../constants/colors";
+import {MEALS} from "../../data/dummydata";
+import {Ionicons} from "@expo/vector-icons";
+import React from "react";
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 function MealPlan() {
+
+    const maxImages = 4;
+    const limitedMeals = MEALS.slice(0, maxImages);
 
 
     return (
@@ -14,29 +23,67 @@ function MealPlan() {
             </View>
 
             <View style={styles.InnerContainer}>
-                <Text style={styles.MealHeader}> Breakfast </Text>
+
+                <View style={styles.MealHeader}>
+                <Text style={styles.MealHeaderText}> Breakfast </Text>
+                    <Text style={styles.MealHeaderClock}> <Ionicons
+                        name={'time'}
+                        color="black"
+                        size={20}
+                    /> 05.00am-07.00am </Text>
+                </View>
+
                 <View style={styles.MealContainer}>
-                    <Text> MealPictures + addImage </Text>
+
+                    {limitedMeals.map((meal) => (
+                        <View style={styles.card} key={meal.id}>
+                            <Image source={{ uri: meal.imageUrl }} style={styles.image} />
+                        </View>
+                    ))}
 
                     <Text style={styles.MealText}> 380 kcal </Text>
                 </View>
             </View>
 
             <View style={styles.InnerContainer}>
-                <Text style={styles.MealHeader}> Lunch </Text>
+                <View style={styles.MealHeader}>
+                    <Text style={styles.MealHeaderText}> Lunch </Text>
+                    <Text style={styles.MealHeaderClock}>  <Ionicons
+                        name={'time'}
+                        color="black"
+                        size={20}
+                    /> 12.30pm-01.00pm </Text>
+                </View>
                 <View style={styles.MealContainer}>
-                    <Text> MealPictures + addImage </Text>
 
-                    <Text style={styles.MealText}> 420 kcal </Text>
+                    {limitedMeals.map((meal) => (
+                        <View style={styles.card} key={meal.id}>
+                            <Image source={{ uri: meal.imageUrl }} style={styles.image} />
+                        </View>
+                    ))}
+
+                    <Text style={styles.MealText}> 380 kcal </Text>
                 </View>
             </View>
 
             <View style={styles.InnerContainer}>
-                <Text style={styles.MealHeader}> Dinner </Text>
+                <View style={styles.MealHeader}>
+                    <Text style={styles.MealHeaderText}> Dinner </Text>
+                    <Text style={styles.MealHeaderClock}>  <Ionicons
+                        name={'time'}
+                        color="black"
+                        size={20}
+                    /> 06.00pm-08.00pm </Text>
+                </View>
                 <View style={styles.MealContainer}>
-                    <Text> MealPictures + addImage </Text>
 
-                    <Text style={styles.MealText}> 220 kcal </Text>
+                    {limitedMeals.map((meal) => (
+                        <View style={styles.card} key={meal.id}>
+                            <Image source={{ uri: meal.imageUrl }} style={styles.image} />
+                        </View>
+                    ))}
+
+                    <Text style={styles.MealText}> 380 kcal </Text>
                 </View>
             </View>
 
@@ -69,7 +116,22 @@ const styles = StyleSheet.create({
     Container: {
         flex: 1,
     },
-
+    card: {
+        width: windowWidth * 0.2,
+        height: windowHeight * 0.1,
+        borderRadius: 20,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 4,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+        backgroundColor: 'white',
+        marginRight: -50,
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 8,
+    },
     Calender: {
         flex: 0.25,
         backgroundColor: COLORS.white,
@@ -90,11 +152,12 @@ const styles = StyleSheet.create({
         shadowColor: 'black',
         shadowOpacity: 0.25,
         shadowOffset: { width: 0, height: 2 },
-        borderRadius: 10,
+        borderRadius: 20,
         elevation: 4,
         shadowRadius: 8,
         padding: 8,
-        margin: 10,
+        marginHorizontal: 10,
+        marginVertical: 2.5,
     },
 
     CalorieInnerContainer: {
@@ -121,9 +184,9 @@ const styles = StyleSheet.create({
     MealContainer: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+        width: '100%',
     },
+
 
     CalorieHeader: {
         flexDirection: 'row',
@@ -136,16 +199,35 @@ const styles = StyleSheet.create({
         fontSize: 18,
     },
 
-
     MealHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    MealHeaderText: {
         fontWeight: "bold",
         fontSize: 18,
+    },
+
+    MealHeaderClock: {
+
+        fontSize: 15
     },
 
     MealText: {
         fontWeight: "bold",
         fontSize: 18,
-    }
+        marginLeft: 120,
+        marginTop: 30,
+    },
+    image: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+        borderRadius: 20,
+        backgroundColor: 'yellow',
+    },
 });
 
 export default MealPlan;
